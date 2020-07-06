2e1622dbaa5d5acc639b6db95c7a15d4cb9b9fe6
Nikulin Sergey <dark_said@mail.ru>
2020-07-06 21:56:21 +0300

    first commit

---
+++ JUnit/src/test/java/SampleTests.java
@@ -0,0 +1,41 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SampleTests {
+    protected static WebDriver driver;
+    private Logger logger = LogManager.getLogger(SampleTests.class);
+
+    @Test
+    public void log(){
+        logger.info("Info log");
+        logger.warn("Warn log");
+        logger.debug("Debug log");
+        logger.fatal("Fatal log");
+        logger.error("Error log");
+    }
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        logger.info("Драйвер поднят");
+    }
+    @Test
+    public void openPage() {
+        driver.get("https://otus.ru/");
+        logger.info("Открыта страница отус");
+    }
+
+    @After
+    public void setDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class Main {
+    public static void main(String args[]){
+        //System.setProperty("webdriver.chrome.driver", "/Users/u17364669/IdeaProjects/OtusLearning/src/main/resources/WebDrivers/chromedriver");
+        //WebDriver webDriver = new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver", "/Users/u17364669/IdeaProjects/OtusLearning/src/main/resources/WebDrivers/geckodriver");
+        WebDriver webDriver = new FirefoxDriver();
+
+        webDriver.get("https://otus.ru");
+    }
+}
