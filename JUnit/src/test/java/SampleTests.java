53eaf5bb09274c52e46d5c198c9225a2b99ef8df
Nikulin Sergey <dark_said@mail.ru>
2020-07-06 22:28:00 +0300

    add owner

--- JUnit/src/test/java/SampleTests.java
+++ JUnit/src/test/java/SampleTests.java
@@ -1,4 +1,6 @@
+import config.ServerConfig;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.aeonbits.owner.ConfigFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.After;
@@ -10,15 +12,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SampleTests {
     protected static WebDriver driver;
     private Logger logger = LogManager.getLogger(SampleTests.class);
+    private ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
 
-    @Test
-    public void log(){
+   /* @Test
+    public void log() {
         logger.info("Info log");
         logger.warn("Warn log");
         logger.debug("Debug log");
         logger.fatal("Fatal log");
         logger.error("Error log");
-    }
+    }*/
 
     @Before
     public void setUp() {
@@ -26,16 +29,18 @@ public class SampleTests {
         driver = new ChromeDriver();
         logger.info("Драйвер поднят");
     }
+
     @Test
     public void openPage() {
-        driver.get("https://otus.ru/");
-        logger.info("Открыта страница отус");
+        driver.get(cfg.url());
+        logger.info("Открыта страница " + cfg.url());
     }
 
     @After
     public void setDown() {
         if (driver != null) {
             driver.quit();
+            logger.info("Закрытие браузера");
         }
     }
 }
